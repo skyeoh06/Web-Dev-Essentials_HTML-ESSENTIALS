84ecf4bc3cee3ebac50d8c6ef51326b2512fb632
skyeoh06 <61610325+skyeoh06@users.noreply.github.com>
2024-07-24 15:45:31 +0800

    Create 1 LAB.py

---
+++ 1 LAB.py
@@ -0,0 +1,38 @@
+#The Binary System
+#Practice
+#Convert the following binary numbers to decimal numbers:
+
+1101 >>
+The binary number 1101 is equivalent to the decimal number of 13:
+
+(1 × 20) + (0 × 21) + (1 × 22) + (1 × 23) = 1 + 0 + 4 + 8 = 13
+
+100101 >>
+The binary number 100101 is equivalent to the decimal number of 37:
+
+(1 × 20) + (0 × 21) + (1 × 22) + (0 × 23) + (0 × 24) + (1 × 25) = 1 + 0 + 4 + 0 + 0 + 32 = 37
+
+11111111 >>
+The binary number 11111111 is equivalent to the decimal number 255:
+
+(1 × 20) + (1 × 21) + (1 × 22) + (1 × 23) + (1 × 24) + (1 × 25) + (1 × 26) + (1 × 27) = 1 + 2 + 4 + 8 + 16 + 32 + 64 + 128 = 255
+
+#The Hexadecimal System
+#Practice
+#Convert the following hexadecimal numbers to decimal numbers:
+
+4A >>
+The hexadecimal number 4A is equivalent to the decimal number of 74:
+
+(4 × 160) + (10 × 161) = 4 + 160 = 74
+
+FF >> 
+The hexadecimal number FF is equivalent to the decimal number of 255:
+
+(15 × 160) + (15 × 161) = 15 + 240 = 255
+
+1B7 >>
+The hexadecimal number 1B7 is equivalent to the decimal number of 439:
+
+(7 × 160) + (11 × 161) + (1 × 162) = 7 + 176 + 256 = 439
+
